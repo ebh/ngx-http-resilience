4645696d0a6a5bcdcd14e2e367b00f0cba27ad30
Alex Dess <alex.dess@agriwebb.com>
2023-08-24 07:32:23 +1000

    Fix linting

--- packages/example/src/app/app.component.spec.ts
+++ packages/example/src/app/app.component.spec.ts
@@ -21,6 +21,6 @@ describe('AppComponent', () => {
   it(`should have as title 'example'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('example');
+    expect(app.title).toBe('example');
   });
 });
