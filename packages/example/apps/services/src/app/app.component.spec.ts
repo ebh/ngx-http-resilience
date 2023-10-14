6cde435ba56f2bcc4807ad87a2db254be62b9382
Alex Dess <alex.dess@agriwebb.com>
2023-10-15 06:28:25 +1100

    Fix failing tests

--- packages/example/apps/services/src/app/app.component.spec.ts
+++ packages/example/apps/services/src/app/app.component.spec.ts
@@ -1,26 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent],
-    }).compileComponents();
-  });
+  it('should create app with example-common and title', async () => {
+    const { fixture } = await render(AppComponent);
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome example-apps-services'
-    );
-  });
+    // Expect example-common to be rendered
+    expect(screen.getByText('example-common works!')).toBeVisible();
 
-  it(`should have as title 'example-apps-services'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toBe('example-apps-services');
+    // Expect title to be set
+    expect(fixture.componentInstance.title).toBe('example-apps-services');
   });
 });
