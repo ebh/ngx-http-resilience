4bf8717582821697a33936bbe935ed9dbb71ab7e
Alex Dess <alex.dess@agriwebb.com>
2023-11-09 22:06:51 +1100

    Export retry interceptor service

--- packages/ngx-http-resilience/src/lib/retry/index.ts
+++ packages/ngx-http-resilience/src/lib/retry/index.ts
@@ -1,6 +1,7 @@
 export * from './delay-fns';
 export * from './error-predicates';
 export * from './http-retry-interceptor.fn';
+export * from './http-retry-interceptor.service';
 export * from './predicate-builder';
 export * from './request-predicates';
 export * from './types';
