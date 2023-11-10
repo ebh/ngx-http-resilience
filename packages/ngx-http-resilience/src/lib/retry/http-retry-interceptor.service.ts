ebd144ade693316db53b9399b4cb533370f4c1ed
Alex Dess <alex.dess@agriwebb.com>
2023-11-11 08:18:09 +1100

    Able to create retry interceptor service

--- packages/ngx-http-resilience/src/lib/retry/http-retry-interceptor.service.ts
+++ packages/ngx-http-resilience/src/lib/retry/http-retry-interceptor.service.ts
@@ -4,16 +4,18 @@ import {
   HttpInterceptorFn,
   HttpRequest,
 } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { createHttpRetryInterceptorFn } from './http-retry-interceptor.fn';
 import { RetryPolicy } from './types';
 
-@Injectable()
 export class HttpRetryInterceptorService implements HttpInterceptor {
   private readonly interceptorFn: HttpInterceptorFn;
 
-  constructor(policy: RetryPolicy) {
+  private constructor(policy: RetryPolicy) {
     this.interceptorFn = createHttpRetryInterceptorFn(policy);
+  }
+
+  public static create(policy: RetryPolicy) {
+    return new HttpRetryInterceptorService(policy);
   }
 
   public intercept(req: HttpRequest<unknown>, next: HttpHandler) {
